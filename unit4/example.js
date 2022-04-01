5db4c7b24d5ac6e4bbd793817fd3f12a03c76aaf
sungjihyun <jhsung23@gmail.com>
2022-04-01 16:23:07 +0900

    4장 변수

---
+++ unit4/example.js
@@ -0,0 +1,36 @@
+//ex04-02
+var userId=1;
+var userName='Lee';
+
+var user={id:1, name:'Lee'};
+var users=[
+    {id:1, name:'Lee'},
+    {id:2, name:'Kim'}
+];
+
+//ex04-03
+var res=10+20;
+console.log(res);
+
+//ex04-04
+var score; //score에 값을 할당하지 않았으므로 undefined라는 값이 할당되어 초기화된다.
+var re=undefined;
+console.log(score); //undefined
+
+//ex04-05
+console.log(example);
+var example; //변수 선언문보다 변수 참조 코드가 앞에 있는데도 실행된다.
+
+//ex04-08
+console.log(num);
+
+var num;
+num=80;
+
+console.log(num);
+
+//ex04-10
+console.log(num2);
+num2=80;
+var num2;
+console.log(num2);
